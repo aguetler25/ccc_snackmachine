3f50eddf5c8f7ba8a6a95edd563c920ce99ff6e2
aguetler25 <aguetler25@icapital.at>
2018-04-11 13:54:07 +0200

    Testdaten einlesen

--- Level1.java
+++ Level1.java
@@ -2,8 +2,14 @@
 public class Level1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int betrag = Integer.parseInt(args[0]); 
+		int anzahlMuenzen = Integer.parseInt(args[1]);
+		int[] muenzen = new int[anzahlMuenzen];
+		
+		for (int m = 0; m < anzahlMuenzen; m++) {
+			int offset = 2;
+			muenzen[m] = Integer.parseInt(args[offset+m]);
+		}
 	}
 
 }
